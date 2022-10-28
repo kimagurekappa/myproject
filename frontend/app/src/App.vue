14047ec64aede9caf6cd65f9fcb7ac515d4f3cb0
kimagurekappa <tomoya0799@gmail.com>
2022-10-28 19:01:13 +0900

    sub menu

--- frontend/app/src/App.vue
+++ frontend/app/src/App.vue
@@ -19,9 +19,43 @@ export default {
     <el-header>きまぐれかっぱ</el-header>
     <el-container>
       <el-aside width="200px">
-        <el-menu default-active="home" router>
-          <el-menu-item index="home" :route="{ name:'home' , path: './' }">Home</el-menu-item>
-          <el-menu-item index=about :route="{ name:'about' , path: './' }">About</el-menu-item>
+        <el-menu :default-openeds="['1','2','3']">
+          <el-sub-menu index="1">
+            <template #title>
+              <el-icon><message /></el-icon>Home
+            </template>
+            <el-menu-item-group>
+              <el-menu default-active="home" router>
+              <!-- <template #title>Home</template> -->
+              <el-menu-item index="home" :route="{ name:'home' , path: './' }">Home</el-menu-item>
+              <el-menu-item index="about" :route="{ name:'about' , path: './' }">About</el-menu-item>
+            </el-menu>
+            </el-menu-item-group>
+            <!-- <el-sub-menu index="1-4">
+              <template #title>Option4</template>
+              <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
+            </el-sub-menu> -->
+          </el-sub-menu>
+          <el-sub-menu index="2">
+            <template #title>
+              <el-icon><message /></el-icon>株価
+            </template>
+            <el-menu-item-group>
+              <!-- <template #title>Home</template> -->
+              <el-menu-item index="2-1">Option 1</el-menu-item>
+              <el-menu-item index="2-2">Option 2</el-menu-item>
+            </el-menu-item-group>
+          </el-sub-menu>
+          <el-sub-menu index="3">
+            <template #title>
+              <el-icon><message /></el-icon>競馬
+            </template>
+            <el-menu-item-group>
+              <!-- <template #title>Home</template> -->
+              <el-menu-item index="3-1">Option 1</el-menu-item>
+              <el-menu-item index="3-2">Option 2</el-menu-item>
+            </el-menu-item-group>
+          </el-sub-menu>
         </el-menu>
       </el-aside>
       <el-main>
@@ -65,5 +99,28 @@ body > .el-container {
 
 .el-container:nth-child(7) .el-aside {
   line-height: 320px;
+}
+
+.layout-container-demo .el-header {
+  position: relative;
+  background-color: var(--el-color-primary-light-7);
+  color: var(--el-text-color-primary);
+}
+.layout-container-demo .el-aside {
+  color: var(--el-text-color-primary);
+  background: var(--el-color-primary-light-8);
+}
+.layout-container-demo .el-menu {
+  border-right: none;
+}
+.layout-container-demo .el-main {
+  padding: 0;
+}
+.layout-container-demo .toolbar {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  right: 20px;
 }
 </style>
--- frontend/app/src/components/About.vue
+++ frontend/app/src/components/About.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h3>ページの更新をすると Home に戻らなーーーーーーい！</h3>
+    <iframe width="600" height="800" src="https://datastudio.google.com/embed/reporting/9c61d730-10ca-4b06-b469-e7c17c0f6606/page/p_xut5obeexc" frameborder="0" style="border:0" allowfullscreen></iframe>
   </div>
 </template>
 
